eb8d9f4e172a1dd1506e2dcaed30f75a6c248565
emperator <emperator@android>
2017-02-22 06:51:58 -0400

    adding a ViewPager to swiping between activities

---
+++ app/src/main/java/com/example/android/miwok/PagerFragmentAdapter.java
@@ -0,0 +1,39 @@
+package com.example.android.miwok;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+/**
+ * Created by emperator on 21/02/2017.
+ */
+
+public class PagerFragmentAdapter extends FragmentPagerAdapter {
+
+    public PagerFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int position) {
+        if(position ==0){
+            return new NumbersFragment();
+        }
+        else if(position==1){
+            return new ColorsFragment();
+        }
+        else if(position==2){
+            return new FamilyFragment();
+        }
+        else if(position==3){
+            return new PhrasesFragment();
+        }
+
+     return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 4;
+    }
+}
